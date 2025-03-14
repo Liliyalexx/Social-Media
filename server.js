6db9ff82b1e53bdf5af6d07feef6d508557184d7
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-14 14:13:33 -0700

    wip: post and users added to server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require('express');
+const mongoose = require("mongoose");
+const User = require('./models/user');
+const Post = require('./models/post');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Visionary App!');
