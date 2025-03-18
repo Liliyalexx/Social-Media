653106ca3b059ee69a4433b279468389891bd657
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-17 20:50:44 -0700

    wip: likes and comments added to post Schema

--- models/posts.js
+++ models/posts.js
@@ -1,3 +1,20 @@
+// const mongoose = require('mongoose');
+
+// const postSchema = new mongoose.Schema({
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref : 'User'
+//     },
+//     title: String,
+//     description: String,
+//     image: String
+// });
+
+
+// module.exports = mongoose.model('Post', postSchema);
+
+// const mongoose = require("mongoose");
+
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
@@ -7,9 +24,17 @@ const postSchema = new mongoose.Schema({
     },
     title: String,
     description: String,
-    image: String
+    image: String,
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        text: String,
+        createdAt: { type: Date, default: Date.now }
+    }]
 });
-
 
 module.exports = mongoose.model('Post', postSchema);
 
