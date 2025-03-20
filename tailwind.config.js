df7002f575300141bba8fdc3d4c3996fdb692de2
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-20 14:42:12 -0700

    Fix Tailwind CSS build for Heroku

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/**/*.ejs", 
-    "./public/**/*.js",
+    "./views/**/*.ejs", // Add paths to all your template files
+    "./public/**/*.js", // Path to your JavaScript files (if any)
   ],
   theme: {
     extend: {},
